c298767b4c52993513c0ee0c622216874f2cebd8
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-06-06 12:15:30 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 
 
     public Board(Integer rows, Integer columns) {
+        if(rows < 1 ||columns < 1){
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         pieces = new Piece[rows][columns];
         this.rows = rows;
         this.columns = columns;
@@ -24,15 +27,34 @@ public class Board {
     }
 
     public Piece piece(int row, int column){
+        if(positionExists(new Position(row, column))){
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
     public Piece piece(Position position){
+        if(positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+    public boolean positionExists(Position position){
+        return position.getRow() < 0 || position.getColumn() < 0
+                || position.getRow() >= this.rows || position.getColumn() >= this.columns;
+    }
+    public boolean thereIsAPiece(Position position){
+        if(positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+       return piece(position) != null;
     }
 
     @Override
